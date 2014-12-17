e6a4829662b04c7216d64e2f984aaf0092f66072
Josh Benson <bensonjosha@gmail.com>
2014-12-17 13:10:49 -0500

    Made game playable very basically

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var spaces = [
 var player1 = 'veggies';
 var player2 = 'junkfood';
 var currentPlayer = null;
-
+var GO = NaN;
 var setNextTurn = function () {
   if (currentPlayer === player1) {
     currentPlayer = player2;
@@ -17,7 +17,7 @@ var setNextTurn = function () {
   else {
     currentPlayer = player1;
   }
-  $('#turn-label').text(currentPlayer);
+  $('#turn-label').text("It's " + currentPlayer + "'s turn...");
 };
 
 var checkForWinner = function () {
@@ -28,30 +28,81 @@ var checkForWinner = function () {
   if ( spaces[0] === spaces[1] && spaces[1] === spaces[2]
     || spaces[3] === spaces[4] && spaces[4] === spaces[5]
     || spaces[6] === spaces[7] && spaces[7] === spaces[8]
+    || spaces[0] === spaces[3] && spaces[3] === spaces[6]
+    || spaces[1] === spaces[4] && spaces[4] === spaces[7]
+    || spaces[2] === spaces[5] && spaces[5] === spaces[8]
+    || spaces[0] === spaces[4] && spaces[4] === spaces[8]
+    || spaces[2] === spaces[4] && spaces[4] === spaces[6]
     // TODO: Check for rest of game winning cases
   )
   {
     console.log('Somebody won!');
-    // TODO: Handle game winner
+    
+    onGameWin(currentPlayer);
+    return true
   }
 };
 
 $(document).on('click', '#board .space', function (e) {
+  var validMove = NaN
+
   var spaceNum = $(e.currentTarget).index();
-  console.log('You clicked on space #' + spaceNum);
+  
+    console.log('You clicked on space #' + spaceNum);
 
   // Marks the space with the current player's name
-  spaces[spaceNum] = currentPlayer;
-  // Adds a class to elem so css can take care of the visuals
-  $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
 
-  checkForWinner();
-  setNextTurn();
+    if (spaces[spaceNum] || GO===true) { console.log('Nope...'); }
+
+     else { spaces[spaceNum] = currentPlayer;
+
+      validMove = true;
+
+    }
+
+
+  // Adds a class to elem so css can take care of the visuals
+
+  if (validMove === true) {
+
+                  $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
+
+                  if (checkForWinner()) {
+                    GO = true
+                  }
+                    else {
+
+                 
+
+                  setNextTurn();};}
 });
 
 function onGameWin (winner) {
-  // TODO: Alert who won the game
-}
+
+  console.log(winner + ' won the game!');
+
+  $('#turn-label').text("WINNER: " + winner);
+ 
+  
+  $(document).ready(function(){
+ 
+var ex = 0;
+do {
+
+
+   
+    if (winner==='veggies') {
+      $('#board .space.veggies').animate({  opacity:'0.6'},"slow");
+  } else{
+    $('#board .space.junkfood').animate({  opacity:'0.6'},"slow");
+  }
+$('#board .space.veggies').animate({  opacity:'1.0'},"slow");
+  $('#board .space.junkfood').animate({  opacity:'1.0'},"slow");
+    ex +=1;    } while (ex<1000);
+  });
+
+ 
+ };
 
 // Start the game
 setNextTurn();
